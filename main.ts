29cedd0a9948c59b85dfd7f2b40a3331ebf6b6d9
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-16 09:44:18 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,17 @@
+// Question no 44 
+function orderSandwich(...items: string[]){
+    if (items.length === 0) {
+      console.log("You haven't selected any sandwich items.");
+    } else {
+      console.log("Sandwich Summary:");
+      items.forEach((item, index) => {
+        console.log(`${index + 1}. ${item}`);
+      });
+    }
+  }
+  
+  // Call the function with different arguments
+  orderSandwich("Turkey", "Lettuce", "Tomato", "Mayo");
+  orderSandwich("Ham", "Cheese");
+  orderSandwich("Peanut Butter", "Jelly", "Banana");
+  
